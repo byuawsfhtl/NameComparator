cea73419a494e9e1889404cf6831faf8d38c7a8e
Emaniacinator <TeamVinton5@gmail.com>
2026-04-01 15:48:27 -0600

    Started working on cleaning list of names

--- NameComparator/NameComparator.py
+++ NameComparator/NameComparator.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import NamedTuple
 from json import loads as json_loads
 from importlib.resources import files
+from pydantic import BaseModel
+from re import split as re_split
 
 from NameComparator.src.clean import clean_name, clean_names_by_comparison, clean_ipa
 from NameComparator.src.nicknames import remove_nicknames
@@ -61,7 +63,7 @@ class ResultsOfNameComparison:
     most_recent_attempt_score: float = 0
     average_score_of_combined_attempts: float = 0
 
-class FlexibleName:
+class FlexibleName(BaseModel):
     """ This class represents a name in a way that allows for more
     flexible comparison and for determining the most complete and full
     version of a name when given a list of possible name inputs.
@@ -76,6 +78,10 @@ class FlexibleName:
         extrapolated_name: The complete name as extrapolated from
             all of the name variations in the name list
     """
+
+    def __init__(self, name_list_input: list[str]):
+        self.original_name_list = name_list_input
+        self.cleaned_name_list = clean_name_list(name_list_input)
     # TODO: Build functions to allow for all of these different variables and
     # outputs to be determined, probably when the class is constructed. Also
     # allow for different constructor variants if that feels appropriate
@@ -174,3 +180,154 @@ def compare_two_names(name_one:str, name_two:str, frequency_data:FrequencyData|N
         results.most_recent_attempt_score = attempt_four_score
         results.average_score_of_combined_attempts = ((attempt_four_score + attempt_three_score + attempt_two_score + attempt_one_score) / 4)
     return results
+
+
+def find_best_name_in_list(cleaned_list_of_names) -> str:
+    
+    # If there is nothing left in the cleaned names, we can't
+    # determine the best name so return an empty string
+    if not cleaned_list_of_names:
+        return ''
+    
+    # If there is only one name in the cleaned names, we can
+    # safely say that's the best name in the list. Just
+    # return it
+    if len(cleaned_list_of_names) == 1:
+        return cleaned_list_of_names[0]
+    
+    broken_name_list = []
+    current_index_in_name_list = 0
+    for name in cleaned_list_of_names:
+
+        # Split the name by likely indicators of different names (eg, surname, first name, etc)
+        split_name = re_split(r'[. ,]\s*', name)
+
+        # Create a dictionary to add to a list for later comparison of fragments
+        dictionary_to_add_to_broken_name_list = {
+            'complete_name': name,
+            'complete_name_position_in_list': current_index_in_name_list,
+            'total_fragments': len(split_name),
+            'fragment_list': []
+        }
+
+        # Remove spaces, commas, and periods from name fragments to get accurate info on them
+        for name_fragment in split_name:
+            name_fragment.strip()
+            name_fragment = name_fragment.replace('.', '')
+            name_fragment = name_fragment.replace(',', '')
+            length_of_fragment = len(list(name_fragment))
+
+            # If the fragment isn't just an initial, we want to factor it into the best name
+            # decision. Add it to the list of fragments in the name
+            if length_of_fragment >= 1:
+                fragment_to_add = {
+                    'fragment_string': name_fragment,
+                    'fragment_length': length_of_fragment
+                }
+                dictionary_to_add_to_broken_name_list['fragment_list'].append[fragment_to_add]
+
+        # Update this so we know where we are in the cleaned list of names    
+        current_index_in_name_list = current_index_in_name_list + 1
+
+        # Add the fully constructed dictionary to the list of broken up names
+        broken_name_list.append(dictionary_to_add_to_broken_name_list)
+
+    # Compare items in the list of broken up names to determine what is going to be the best name
+    # from the entire list
+    for item in broken_name_list:
+        apple = 0
+
+
+def clean_name_list(input_list_of_names) -> list[str]:
+
+    list_of_matches = []
+    list_of_non_matches = []
+    index_count = 0
+
+    # If the list is 2 items long or less, it will be inconclusive since we can't determine
+    # which names are going to be the most significant using this method so return an empty
+    # list of matches. This also establishes a base case for recursion, which is important
+    if input_list_of_names.length <= 2:
+        return []
+
+    for item in input_list_of_names:
+
+        # Skip the first name because there isn't anything to compare to yet
+        if index_count == 0:
+            index_count = index_count + 1
+            continue
+
+        # For the second name, compare it to the first name
+        elif index_count == 1:
+            result_of_comparison = compare_two_names(input_list_of_names[0], item).match
+            # If they're a match, start building a list of matches
+            if result_of_comparison:
+                list_of_matches.append(input_list_of_names[0])
+                list_of_matches.append(item)
+            # If they're not a match, start building a list of non-matches
+            else:
+                list_of_non_matches.append(input_list_of_names[0])
+                list_of_matches.append(item)
+            index_count = index_count + 1
+
+        # After the second name we can apply this logic repeatedly
+        else:
+            # If there haven't been any matches, compare to the list of non-matches
+            # to see if this name matches any of them
+            if not list_of_matches:
+
+                got_an_initial_match = False
+
+                for not_a_match in list_of_non_matches:
+
+                    # If we already found a basis from which to create a match list, we don't need to run this anymore
+                    if got_an_initial_match:
+                        break
+
+                    result_of_comparison_with_non_match = compare_two_names(item, not_a_match).match
+                    # If the name matches something in the list of non-matches so far, add them to the
+                    # list of matches
+                    if result_of_comparison_with_non_match:
+                        got_an_initial_match = True
+                        list_of_matches.append(not_a_match)
+                        list_of_matches.append(item)
+                        list_of_non_matches.remove(not_a_match)
+
+                # If there are still no more matches, add the name to the list of non-matches since it didn't match anything
+                if not got_an_initial_match:
+                    list_of_non_matches.append(item)
+
+            # Otherwise, compare the new name to the list of matches to see if it
+            # matches any of the ones there
+            else:
+
+                matches_another_match = False
+
+                for already_a_match in list_of_matches:
+
+                    # If it already matches something, we can just skip the rest of the cycle
+                    if matches_another_match:
+                        break
+
+                    result_of_comparison_with_match = compare_two_names(item, already_a_match).match
+                    # If the name matches another match, add it into the list of matches
+                    if result_of_comparison_with_match:
+                        matches_another_match = True
+                        list_of_matches.append(item)
+
+                # If the name doesn't match any of the other matches, add it to the list of non-matches
+                if not matches_another_match:
+                    list_of_non_matches.append(item)
+
+    # Run this function again on the list of non-matches to make sure that there isn't a better list
+    # of matches that could be returned
+    check_for_better_match_list = clean_name_list(list_of_non_matches)
+
+    # If the returned match list has more matches than the initial match list, determine that one
+    # is the better match between the two since it matches more things. Return that one instead
+    if (len(check_for_better_match_list) > len(list_of_matches)):
+        list_of_matches = check_for_better_match_list
+
+    return list_of_matches
+
+
